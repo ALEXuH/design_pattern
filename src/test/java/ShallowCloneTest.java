5e10423856cbb42aea26d319009d6b65d2863c33
ALEXuH <xuzhiceng@Gmail.com>
2019-05-09 19:45:35 +0800

    add prototype pattern

---
+++ src/test/java/ShallowCloneTest.java
@@ -0,0 +1,30 @@
+import pattern.Prototype.Circle;
+import pattern.Prototype.ShallowClone;
+
+/** Created by ALEXuH on 2019/5/9 0009 */
+public class ShallowCloneTest {
+
+  public static void main(String[] args) {
+    // 浅克隆
+    try {
+      ShallowClone sha = new ShallowClone();
+      ShallowClone shaClone = sha.clone();
+      System.out.println(sha == shaClone);
+      System.out.println(sha.equals(shaClone));
+    } catch (CloneNotSupportedException e) {
+      System.out.println(e.getMessage());
+    }
+
+    // 深克隆
+    Circle cir = new Circle();
+    cir.x = 1;
+    cir.y = 90;
+    cir.color = "red";
+    cir.radius = 10;
+    System.out.println(cir.y);
+
+    Circle cir1 = (Circle) cir.clone();
+    System.out.println(cir.equals(cir1));
+    System.out.println("cir1：" + cir.hashCode() + " | cir2: " + cir1.hashCode());
+  }
+}
