7e6ae1d4b0e13263a500d96eb0b5ba9fccc43dfc
ALEXuH <xuzhiceng@Gmail.com>
2019-10-08 10:41:58 +0800

    add invoker

--- src/main/java/pattern/behavioral/commond/Invoker.java
+++ src/main/java/pattern/behavioral/commond/Invoker.java
@@ -4,4 +4,15 @@ package pattern.behavioral.commond;
  * Created by ALEXuH on 2019/7/4 0004
  */
 public class Invoker {
+
+  private Commond commond;
+
+  public Invoker(Commond commond) {
+    this.commond = commond;
+  }
+
+  public void process() {
+    commond.exec();
+  }
+
 }
